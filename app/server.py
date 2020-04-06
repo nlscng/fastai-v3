999699823ddaac55e729cec3961af8353cffbeb6
nelson cheng <nlscng.g@gmail.com>
2020-04-05 19:26:41 -0500

    Trying out free deploy on render with the sweet predictor, copy/modified from fast.ai black bear predictor.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=18FR1NjOu0PJ06OW4Mp7D6W4DI523bEGN'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['pancake', 'waffle', 'crepe']
 path = Path(__file__).parent
 
 app = Starlette()
